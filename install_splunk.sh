f0b1b5afd5bf47fb56fcd4848c4c8cf318d89f48
maar4569 <root@atsux.(none)>
2015-11-09 18:14:30 +0900

    add install shell.

---
+++ install_splunk.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+export SPLUNK_RPM_PKG=$1
+ecport $SPLUNK_HOME=/opt/splunk
+
+echo 1.change ACL 
+sudo chmod 744 $SPLUNK_RPM_PKG
+
+echo 2.install splunk
+sudo rpm -ivh $SPLUNK_RPM_PKG
+
+echo 3.service start
+sudo $SPLUNK_HOME/bin/splunk start --answer-yes --no-prompt --accept-license
+sudo $SPLUNK_HOME/bin/splunk enable boot-start -user splunk
+
+echo 4.check status
+cat $SPLUNK_HOME/var/log/splunk/splunkd.log
+ps -A | grep splunk
+
+
+
